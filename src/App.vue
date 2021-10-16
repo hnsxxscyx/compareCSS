6023089a6397890c9e979a88a4b9e7451e774781
Oliver <hnsxxscyx@gmail.com>
2021-10-16 21:17:35 +0800

    feat: add compare css page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
+  import CompareCss from "./pages/CompareCss.vue";
 </script>
 
 <template>
-
+  <CompareCss></CompareCss>
 </template>
 
 <style>
-
 </style>
---
+++ src/pages/CompareCss.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref,watchEffect } from 'vue'
+  import Editer from "../components/Editer.vue";
+  const codeValues = ref({})
+
+const onCodeChange = ({value,name})=>{
+  codeValues.value = {...codeValues.value, name:value};
+}
+watchEffect(()=>{
+  console.log(codeValues.value);
+})
+
+</script>
+
+<template>
+  <div style="height: 80vh; display: flex">
+    <div style="flex: 1">
+      <Editer @codeChange="onCodeChange" name="code-1"></Editer>
+    </div>
+    <div style="flex: 1">
+      <Editer @codeChange="onCodeChange" name="code-2"></Editer>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
